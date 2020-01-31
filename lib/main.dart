9d0df2b81d984e864e198f5e9d8e8e8ba5ad1248
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2020-01-31 23:47:21 +0530

    added an example of problem

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_state_management/Counter.dart';
-import 'package:flutter_state_management/CounterProvider.dart';
-import 'package:provider/provider.dart';
 
-void main() => runApp(ChangeNotifierProvider<CounterProvider>(
-      create: (BuildContext context) =>
-          CounterProvider(counter: Counter(count: 0)),
-      child: MaterialApp(
-        home: MyApp(),
-      ),
+void main() => runApp(MaterialApp(
+      home: MyApp(),
     ));
 
 class MyApp extends StatelessWidget {
+  final count = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("State Management"),
       ),
-      body: FirstClass(),
+      body: FirstClass(count: count,),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
-              context, MaterialPageRoute(builder: (context) => SecondClass()));
+              context, MaterialPageRoute(builder: (context) => SecondClass(count:count,)));
         },
         child: Icon(Icons.arrow_forward_ios),
       ),
@@ -31,6 +25,8 @@ class MyApp extends StatelessWidget {
 }
 
 class FirstClass extends StatefulWidget {
+  final count;
+  FirstClass({this.count});
   @override
   _FirstClassState createState() => _FirstClassState();
 }
@@ -38,15 +34,17 @@ class FirstClass extends StatefulWidget {
 class _FirstClassState extends State<FirstClass> {
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<CounterProvider>(context);
     return Container(
       alignment: Alignment.center,
-      child: Text(provider.counter.count.toString()),
+      child: Text(widget.count.toString()),
     );
   }
 }
 
 class SecondClass extends StatefulWidget {
+  final count;
+  SecondClass({this.count});
+
   @override
   _SecondClassState createState() => _SecondClassState();
 }
@@ -54,11 +52,10 @@ class SecondClass extends StatefulWidget {
 class _SecondClassState extends State<SecondClass> {
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<CounterProvider>(context);
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () => Navigator.push(
-            context, MaterialPageRoute(builder: (context) => ThirdClass())),
+            context, MaterialPageRoute(builder: (context) => ThirdClass(count: widget.count,))),
         child: Icon(Icons.forward),
       ),
       appBar: AppBar(
@@ -66,24 +63,29 @@ class _SecondClassState extends State<SecondClass> {
       ),
       body: Container(
         alignment: Alignment.center,
-        child: Text(provider.counter.count.toString()),
+        child: Text(widget.count.toString()),
       ),
     );
   }
 }
 
 class ThirdClass extends StatefulWidget {
+  final count;
+  ThirdClass({this.count});
+
   @override
-  _ThirdClassState createState() => _ThirdClassState();
+  _ThirdClassState createState() => _ThirdClassState(count);
 }
 
 class _ThirdClassState extends State<ThirdClass> {
+  int count;
+
+  _ThirdClassState(this.count);
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<CounterProvider>(context);
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() => provider.counter.increment()),
+        onPressed: () => setState(() =>count++),
         child: Icon(Icons.add),
       ),
       appBar: AppBar(
@@ -91,7 +93,7 @@ class _ThirdClassState extends State<ThirdClass> {
       ),
       body: Container(
         alignment: Alignment.center,
-        child: Text(provider.counter.count.toString()),
+        child: Text(count.toString()),
       ),
     );
   }
