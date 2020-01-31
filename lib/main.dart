354b31dcc2a8f98917f3b2455d7f0c5d317eb9dd
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2020-01-31 23:55:14 +0530

    class based solution

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_state_management/Counter.dart';
-import 'package:flutter_state_management/CounterProvider.dart';
 
-void main() => runApp(CounterProvider(
-    child: MaterialApp(
-      home: MyApp(),
-    ),
-    counter: Counter(count: 0)));
+
+void main() => runApp(MaterialApp(
+  home: MyApp(),
+));
 
 class MyApp extends StatelessWidget {
+  final counter = Counter(count: 0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("State Management"),
       ),
-      body: FirstClass(),
+      body: FirstClass(count: counter,),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
-              context, MaterialPageRoute(builder: (context) => SecondClass()));
+              context, MaterialPageRoute(builder: (context) => SecondClass(count: counter,)));
         },
         child: Icon(Icons.arrow_forward_ios),
       ),
@@ -28,6 +27,8 @@ class MyApp extends StatelessWidget {
 }
 
 class FirstClass extends StatefulWidget {
+  final count;
+  FirstClass({this.count});
   @override
   _FirstClassState createState() => _FirstClassState();
 }
@@ -35,16 +36,16 @@ class FirstClass extends StatefulWidget {
 class _FirstClassState extends State<FirstClass> {
   @override
   Widget build(BuildContext context) {
-    CounterProvider counterProvider =
-        context.dependOnInheritedWidgetOfExactType<CounterProvider>();
     return Container(
       alignment: Alignment.center,
-      child: Text(counterProvider.counter.count.toString()),
+      child: Text(widget.count.count.toString()),
     );
   }
 }
 
 class SecondClass extends StatefulWidget {
+  final count;
+  SecondClass({this.count});
   @override
   _SecondClassState createState() => _SecondClassState();
 }
@@ -52,12 +53,11 @@ class SecondClass extends StatefulWidget {
 class _SecondClassState extends State<SecondClass> {
   @override
   Widget build(BuildContext context) {
-    CounterProvider counterProvider =
-        context.dependOnInheritedWidgetOfExactType<CounterProvider>();
+
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () => Navigator.push(
-            context, MaterialPageRoute(builder: (context) => ThirdClass())),
+            context, MaterialPageRoute(builder: (context) => ThirdClass(count: widget.count,))),
         child: Icon(Icons.forward),
       ),
       appBar: AppBar(
@@ -65,13 +65,15 @@ class _SecondClassState extends State<SecondClass> {
       ),
       body: Container(
         alignment: Alignment.center,
-        child: Text(counterProvider.counter.count.toString()),
+        child: Text(widget.count.count.toString()),
       ),
     );
   }
 }
 
 class ThirdClass extends StatefulWidget {
+  final count;
+  ThirdClass({this.count});
   @override
   _ThirdClassState createState() => _ThirdClassState();
 }
@@ -79,10 +81,10 @@ class ThirdClass extends StatefulWidget {
 class _ThirdClassState extends State<ThirdClass> {
   @override
   Widget build(BuildContext context) {
-    CounterProvider counterProvider = CounterProvider.of(context);
+
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(()=>counterProvider.counter.increment()),
+        onPressed: () => setState(()=>widget.count.increment()),
         child: Icon(Icons.add),
       ),
       appBar: AppBar(
@@ -90,7 +92,7 @@ class _ThirdClassState extends State<ThirdClass> {
       ),
       body: Container(
         alignment: Alignment.center,
-        child: Text(counterProvider.counter.count.toString()),
+        child: Text(widget.count.count.toString()),
       ),
     );
   }
